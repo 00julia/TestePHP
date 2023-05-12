11f9575b3ea2fabf330ccf8db9658d5323973039
00julia <130668482+00julia@users.noreply.github.com>
2023-05-12 14:35:50 -0300

    ei

--- pagina1.php
+++ pagina1.php
@@ -8,8 +8,18 @@
 </head>
 <body>
     <?php
-            echo "<p> Boa tarde á todos!<p>"
-            
+    //Declaração de variavel
+    $hora = date("H");
+
+    if($hora <12){
+        echo "<p>" .$hora. "Bom Dia! </p>";
+    } else if($hora <18){
+        echo "<p>" .$hora. "Boa Tarde </p>";
+    } else{
+        echo "<p>" .$hora. " Boa Noite! </p>";
+    }
+            // echo "<p> Boa tarde á todos!</p>"
+
     ?> 
 </body>
 </html>

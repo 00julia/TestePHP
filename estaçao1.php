78328695a8f7acb148cea9025b6a181f480ea011
00julia <130668482+00julia@users.noreply.github.com>
2023-05-12 15:15:14 -0300

    origin

--- estaçao1.php
+++ estaçao1.php
@@ -8,6 +8,57 @@
 </head>
 <body>
     <?php
+$dia = ;
+$mes = 12;
+
+switch($mes){
+    case 1:
+    case 2:
+    case 3:
+        if($mes == 3 and $dia >= 20){
+            echo "<p>Outono</p>";
+            echo "<outono.png'>";
+        } else{
+            echo "<p>Verão</p>";
+            echo "<verao.png'>";
+        }
+        break;
+    case 4:
+    case 5:
+    case 6:
+        if($mes == 6 and $dia >= 21){
+            echo "<p>Inverno</p>";
+            echo "<inverno.png'>";
+        }else{
+            echo "<p>Outono</p>";
+            echo "<outono.png'>";
+        }
+        break;
+    case 7:
+    case 8:
+    case 9:
+        if($mes == 9 and $dia >= 23){
+            echo "<p>Primavera</p>";
+            echo "<primavera.png'>";
+        } else {
+            echo "<p>Inverno</p>";
+            echo "<inverno.png'>";
+        }
+        break;
+    case 10:
+    case 11:
+    case 12:
+        if($mes == 12 and $dia >= 22){
+            echo "<p>Verão</p>";
+            echo "<verao.png'>";
+        } else {
+            echo "<p>Primavera</p>";
+            echo "<primavera.png'>";
+        }
+        break;
+    default:
+        echo "<p>Mês " .$mes. " incorreto!</p>";
+}
 
     ?>
 </body>
